89f300a7d104a5bb90f7d71c8324a8fa2775897c
LEEYOENN <rofttm0033@gmail.com>
2025-12-16 20:27:36 +0900

    Feat: 기업 AI TTS 음성 변경

--- src/services/tts_service/elevenlabs_tts_service.py
+++ src/services/tts_service/elevenlabs_tts_service.py
@@ -14,7 +14,11 @@ class ElevenLabsTTSService:
         
         self.client = ElevenLabs(api_key=self.api_key)
         #목소리
-        self.voice_id = "pb3lVZVjdFWbkhPKlelB" #젊은 남성 목소리 - "OEaq3WGNtNvFJ5co9mJE"
+        self.voice_id = "gJSDQIpSQ56NBGhorBfg" # 목소리 좋은 유튜버 목소리
+        # "v1jVu1Ky28piIPEJqRrm" # 젠틀하고낮은 다큐멘터리 남성 
+        #"pb3lVZVjdFWbkhPKlelB" # 중년 목소리 
+        # 젊은 남성 목소리 - "OEaq3WGNtNvFJ5co9mJE"
+
         # 한글 지원 모델
         self.model_id = "eleven_multilingual_v2"
 
